657882aa896f4cef62e39d189f8dbb3f22f79ff2
Mihael <Churin_s@inbox.ru>
2026-07-31 03:10:06 +0200

    Fix config and add role

--- src/main/java/com/example/Api_Gateway/config/SecurityConfig.java
+++ src/main/java/com/example/Api_Gateway/config/SecurityConfig.java
@@ -14,28 +14,14 @@ public class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/login/**", "/oauth2/**").permitAll()
-                        .pathMatchers("/debug/auth/**").authenticated()
-                        .pathMatchers("/debug/roles/**").authenticated()
-                        .pathMatchers("/api/v1/flow-manager/**").authenticated()
+                        .pathMatchers("/api/v1/flow-manager/**").hasRole("user")
+                        .pathMatchers("/api/v1/flow-manager/**").hasRole("admin")
                         .anyExchange().authenticated())
-//                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
-//        return http
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/admin/**")
-//                        .hasRole("ADMIN")
-//                        .pathMatchers("/user/**")
-//                        .hasRole("USER")
-//                        .anyExchange()
-//                        .authenticated())
-//                .oauth2ResourceServer(oauth2 ->
-//                        oauth2.jwt(Customizer.withDefaults()))
-//                .build();
-    }
 
+    }
 }
